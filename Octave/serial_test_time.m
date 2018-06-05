10fc1bb9e7110fa2029d7b8f0d1c2d1f5d41c760
Rob Frohne <rob.frohne@wallawalla.edu>
2018-06-05 10:38:45 -0700

    Changet the IF to 155 Hz, and the octave script serial_test_time.m to match.

--- Octave/serial_test_time.m
+++ Octave/serial_test_time.m
@@ -6,7 +6,7 @@
 
 clc; clear;
 
-fMin = 60e6; % Note:  We need to figure out what the aliased frequency is
+fMin = 10e6; % Note:  We need to figure out what the aliased frequency is
 % and make sure it doesn't interfere with our measurement even with the LPF.
 
 % Load the package
@@ -19,7 +19,7 @@ endif
 % Naturally, set the COM port # to match your device
 % Use this crazy notation for any COM port number: 1 - 255
 %s1 = serial("/dev/pts/2");
-s1 = serial("/tmp/ttyDUMMY"); % $ interceptty /dev/ttyACM0 /tmp/ttyDUMMY
+s1 = serial("/tmp/ttyDUMMY"); % $ 
 %s1 = serial("/dev/ttyACM0"); % This needs debugged.  It is missing data.
 pause(1); % Wait a second as it takes some ports a while to wake up
 % Set the port parameters
@@ -34,11 +34,13 @@ set(s1,'timeout', 255); % 12.3 Seconds as an example here
 %set(s1, 'dataterminalready', 'on'); % Sets the DTR line
 % Optional - Flush input and output buffers
 srl_flush(s1);
-string_to_send = strcat("^SAMPLERATE,","$\n");
-srl_write(s1,string_to_send);
-Fs = str2num(ReadToTermination(s1,10));
-N = str2num(ReadToTermination(s1,10));
-F_IF = str2num(ReadToTermination(s1,10));
+%string_to_send = strcat("^SAMPLERATE,","$\n");
+%srl_write(s1,string_to_send);
+%Fs = str2num(ReadToTermination(s1,10));
+%N = str2num(ReadToTermination(s1,10));
+%F_IF = str2num(ReadToTermination(s1,10));
+F_IF = 155;
+N = 3840/2;
 Fs=8000;  % Fix the frequency correction.
 T = 1/Fs;
 %srl_flush(s1);
@@ -47,8 +49,8 @@ srl_write(s1,string_to_send);
 ref(1,:) = str2num(ReadToTermination(s1,10));
 pause(1);
 meas(1,:)= str2num(ReadToTermination(s1,10));
-ref=ref-mean(ref);
-meas=meas-mean(meas);
+ref=ref-2^13;
+meas=meas-2^13;
 t=0:T:(N-1)*T;
 figure(1)
 plot(t,ref);
@@ -59,20 +61,36 @@ title('Results')
 xlabel('Time (s)')
 figure(2)
 f=-1/2/T:1/N/T:1/2/T-1/N/T;
-Ref = fft(ref);
-Meas = fft(meas);
-Ref(F_IF*N/Fs+1)
-Meas(F_IF*N/Fs+1)
-H1 = Meas(F_IF*N/Fs+1)/Ref(F_IF*N/Fs+1)
-H3 = Meas(3*F_IF*N/Fs+1)/Ref(3*F_IF*N/Fs+1)
-H5 = Meas(5*F_IF*N/Fs+1)/Ref(5*F_IF*N/Fs+1)
-H7 = Meas(7*F_IF*N/Fs+1)/Ref(7*F_IF*N/Fs+1)
-plot(f,abs(fftshift(Ref)),"linewidth",1)
+Ref = fft(hanning(length(ref))'.*ref);
+Meas = fft(hanning(length(meas))'.*meas);
+%Ref(F_IF*N/Fs+1)
+%Meas(F_IF*N/Fs+1)
+%H1 = Meas(F_IF*N/Fs+1)/Ref(F_IF*N/Fs+1)
+%H3 = Meas(3*F_IF*N/Fs+1)/Ref(3*F_IF*N/Fs+1)
+%H5 = Meas(5*F_IF*N/Fs+1)/Ref(5*F_IF*N/Fs+1)
+%H7 = Meas(7*F_IF*N/Fs+1)/Ref(7*F_IF*N/Fs+1)
+%plot(f,abs(fftshift(Ref)),"linewidth",1)
 xlabel('frequency (Hz)')
 title('Reference Response');
 figure(3)
 plot(f,abs(fftshift(Meas)),"linewidth",1)
 xlabel('frequency (Hz)')
+title('Measured Response');\
+figure(4)
+plot(f,20*log10(abs(fftshift(Ref))),"linewidth",1)
+xlabel('frequency (Hz)')
+title('Reference Response');
+figure(5)
+plot(f,20*log10(abs(fftshift(Meas))),"linewidth",1)
+xlabel('frequency (Hz)')
 title('Measured Response');
+figure(6)
+plot(t,ref)
+title('Reference Channel')
+xlabel('Time (s)')
+figure(7)
+plot(t,meas)
+title('Measured Channel')
+xlabel('Time (s)')
 % Finally, Close the port
 fclose(s1);
