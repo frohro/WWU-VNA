f00d6b2240b7aba460ad70c5513d066562f1a5cd
Rob Frohne <rob.frohne@wallawalla.edu>
2018-12-18 21:52:07 -0800

    Added Octave scripts to measure the reflection (with SOL cal).

---
+++ Octave/SOL.m
@@ -0,0 +1,31 @@
+% This function is to do an short, open, load (SOL) one port calibration
+% for vector network analyzer calibration measurements.
+% This is shown 
+% This is worked out in my Electronics II class notes.  Here are some photos of the whiteboard, that outline the procedure. 
+%
+%    The first photo shows how to calculate the reflection in a cascade of two 
+%    scattering matrices.  Here is a link:  (See May 25, 2016.)
+%    http://fweb.wallawalla.edu/~frohro//ClassNotes/ENGR357/2016/Background/index.php
+%    Using this relationship, the second network is simplified into a one port 
+%    so reflection r_2 = Gamma_L, the actual reflection at the DUT.  Then using 
+%    this, the three different reflection coefficients (with DUT as open, short, 
+%    and load), the reflection at the instrument is written down.
+%    These are solved simultaneously to give the three independent parameters 
+%    that describe the unknown scattering matrix between the instrument and DUT.  
+%    (There is no photo of the algebra, just the three equations to start from.)
+%    Then the unknown DUT reflection is measured at the instrument, and the 
+%    reflection at the DUT is solved for.  This reflection is the calibrated 
+%    measurement.
+%
+% The notes don't show all the algebra, but the problem is set up.  The algebra
+% is done in a file in my Classes/Electronics/VNA Calibration One Port SLO.pdf
+% gamma_m is the measured reflection for the DUT and gamma_u is the calibrated
+% gamma returned.
+% gamma_u = SOL(gamma_s, gamma_o, gamma_l, gamma_m);
+
+function gamma_u = SOL(gamma_s, gamma_o, gamma_l, gamma_m)
+  beta = (gamma_o - gamma_l)./(gamma_l -gamma_s); % beta is (1+r')/(1-r')
+  rp = (beta -1)./(beta + 1);
+  alpha = (gamma_l - gamma_s).*(2*beta./(beta+1)); % alpha is t*t' from web.
+  gamma_u = (gamma_m -gamma_l)./((gamma_m -gamma_l).*rp +alpha);
+end
---
+++ Octave/vna_s11_cal.m
@@ -0,0 +1,109 @@
+% VNA 2018 VNA test script.
+% Rob Frohne, May 2017.
+% This script will make nFreq measurements from fMin to fMax.
+% It is designed to make S11 measurements and use calibration.
+% The first step is to connect a reflection bridge to the analyzer
+% and measure the reflection for a short, then an open and then a 50 
+% ohm load, and lastly the DUT (device under test).
+
+clc; clear;
+close all;
+
+fMin = 1.e6;
+fMax = 100.e6;
+nFreq = 10;
+Z0 = 50;
+
+% Load the package
+pkg load instrument-control
+% Check if serial support exists
+if (exist("serial") != 3)
+  disp("No Serial Support");
+endif
+% Instantiate the Serial Port
+% Naturally, set the COM port # to match your device
+% Use this crazy notation for any COM port number: 1 - 255
+%s1 = serial("/dev/pts/2");
+%s1 = serial("/tmp/ttyDUMMY"); % $ interceptty /dev/ttyACM0 /tmp/ttyDUMMY
+s1 = serial("/dev/ttyACM0");
+pause(1); % Wait a second as it takes some ports a while to wake up
+% Set the port parameters
+set(s1,'baudrate', 115200);
+set(s1,'bytesize', 8);
+set(s1,'parity', 'n');
+set(s1,'stopbits', 1);
+set(s1,'timeout', 255); % 12.3 Seconds as an example here
+% Optional commands, these can be 'on' or 'off'
+%set(s1, 'requesttosend', 'on');
+ % Sets the RTS line
+%set(s1, 'dataterminalready', 'on'); % Sets the DTR line
+disp("Connect the short connection and hit return.")
+pause;
+% Optional - Flush input and output buffers
+srl_flush(s1);
+string_to_send = strcat("^SWEEP,",num2str(uint64(fMin)),","...
+                  ,num2str(uint64(fMax)),",",num2str(uint64(nFreq)),"$\n")
+srl_write(s1,string_to_send);
+for i=1:nFreq
+  raw(i,:) = str2num(ReadToTermination(s1, 10));
+  if(mod(i,10) == 0) 
+    disp(i)
+  endif
+endfor
+for i=1:nFreq
+  gamma_s(i) = (raw(i,1)+j*raw(i,2))./(raw(i,3)+j*raw(i,4));
+endfor
+disp("Connect the open connection and hit return.")
+pause;
+% Optional - Flush input and output buffers
+srl_flush(s1);
+string_to_send = strcat("^SWEEP,",num2str(uint64(fMin)),","...
+                  ,num2str(uint64(fMax)),",",num2str(uint64(nFreq)),"$\n")
+srl_write(s1,string_to_send);
+for i=1:nFreq
+  raw(i,:) = str2num(ReadToTermination(s1, 10));
+  if(mod(i,10) == 0) 
+    disp(i)
+  endif
+endfor
+for i=1:nFreq
+  gamma_o(i) = (raw(i,1)+j*raw(i,2))./(raw(i,3)+j*raw(i,4));
+endfor
+disp("Connect the Z_0 load connection and hit return.")
+pause;
+% Optional - Flush input and output buffers
+srl_flush(s1);
+string_to_send = strcat("^SWEEP,",num2str(uint64(fMin)),","...
+                  ,num2str(uint64(fMax)),",",num2str(uint64(nFreq)),"$\n")
+srl_write(s1,string_to_send);
+for i=1:nFreq
+  raw(i,:) = str2num(ReadToTermination(s1, 10));
+  if(mod(i,10) == 0) 
+    disp(i)
+  endif
+endfor
+for i=1:nFreq
+  gamma_l(i) = (raw(i,1)+j*raw(i,2))./(raw(i,3)+j*raw(i,4));
+endfor
+disp("Now connect your DUT, and push return.")
+pause;
+srl_flush(s1);
+string_to_send = strcat("^SWEEP,",num2str(uint64(fMin)),","...
+                  ,num2str(uint64(fMax)),",",num2str(uint64(nFreq)),"$\n")
+srl_write(s1,string_to_send);
+for i=1:nFreq
+  raw(i,:) = str2num(ReadToTermination(s1, 10));
+  if(mod(i,10) == 0) 
+    disp(i)
+  endif
+endfor
+for i=1:nFreq
+  gamma_m(i) = (raw(i,1)+j*raw(i,2))./(raw(i,3)+j*raw(i,4));
+endfor
+% Do calibration.
+gamma_u = SOL(gamma_s, gamma_o, gamma_l, gamma_m);
+Z = Z0*(1+gamma_u)./(1-gamma_u);
+% Plot it on the Smith Chart.
+SmithChart(gamma_u, '-or')
+title('S11');
+fclose(s1);
