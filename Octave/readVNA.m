c851c3f53ec47bc99b4ba20e52b1519163a8fc49
Rob Frohne <rob.frohne@wallawalla.edu>
2018-12-19 21:07:21 -0800

    Added a bit of GUI stuff to the vna_through_cal.m

---
+++ Octave/readVNA.m
@@ -0,0 +1,34 @@
+function data = readVNA(fMin, fMax, nFreq)
+  % This is the function than reads data from the serial port.
+  %s1 = serial("/tmp/ttyDUMMY"); % $ interceptty /dev/ttyACM0 /tmp/ttyDUMMY
+  if(exist(".port","file"))
+    load ".port" port
+  else
+    port = inputdlg({"For example: COM1 or /dev/ttyACM0 "},"Serial Port",[1 10]);
+    save ".port" port
+  endif
+  try
+    serialPort = serial(port{1});
+  catch
+    errordlg("Problem checkinng serial port.")
+    delete(".port")  % In case the port got typed incorrectly.
+  end
+  pause(1); % Wait a second as it takes some ports a while to wake up
+  set(serialPort,'baudrate', 115200);
+  set(serialPort,'bytesize', 8);
+  set(serialPort,'parity', 'n');
+  set(serialPort,'stopbits', 1);
+  set(serialPort,'timeout', 50); 
+  srl_flush(serialPort);
+  f = waitbar(0,"Getting your data...");
+  string_to_send = strcat("^SWEEP,",num2str(uint64(fMin)),","...
+                    ,num2str(uint64(fMax)),",",num2str(uint64(nFreq)),"$\n");
+  srl_write(serialPort,string_to_send);
+  for i=1:nFreq
+    raw(i,:) = str2num(ReadToTermination(serialPort, 10));
+    waitbar(i/nFreq, f);
+  endfor
+  close(f);
+  data = (raw(:,1)+j*raw(:,2))./(raw(:,3)+j*raw(:,4));
+  fclose(serialPort);
+endfunction
--- Octave/vna_through_cal.m
+++ Octave/vna_through_cal.m
@@ -7,46 +7,10 @@
 % It will divide out the transfer function of the cables, so they
 % don't mess up your measurement of the DUT.
 
+
+% Start script here.
 clc; clear;
 close all;
-
-function s21 = readVNA(fMin, fMax, nFreq)
-  % Instantiate the Serial Port
-  % Naturally, set the COM port # to match your device
-  % Use this crazy notation for any COM port number: 1 - 255
-  %s1 = serial("/dev/pts/2");
-  %s1 = serial("/tmp/ttyDUMMY"); % $ interceptty /dev/ttyACM0 /tmp/ttyDUMMY
-  serialPort = serial("/dev/ttyACM0");
-  pause(1); % Wait a second as it takes some ports a while to wake up
-  % Set the port parameters
-  set(serialPort,'baudrate', 115200);
-  set(serialPort,'bytesize', 8);
-  set(serialPort,'parity', 'n');
-  set(serialPort,'stopbits', 1);
-  set(serialPort,'timeout', 255); % 12.3 Seconds as an example here
-  % Optional commands, these can be 'on' or 'off'
-  %set(s1, 'requesttosend', 'on');
-   % Sets the RTS line
-  %set(s1, 'dataterminalready', 'on'); % Sets the DTR line
-  srl_flush(serialPort);
-  string_to_send = strcat("^SWEEP,",num2str(uint64(fMin)),","...
-                    ,num2str(uint64(fMax)),",",num2str(uint64(nFreq)),"$\n")
-  srl_write(serialPort,string_to_send);
-  for i=1:nFreq
-    raw(i,:) = str2num(ReadToTermination(serialPort, 10));
-    if(mod(i,10) == 0) 
-      disp(i)
-    endif
-  endfor
-  s21 = (raw(:,1)+j*raw(:,2))./(raw(:,3)+j*raw(:,4));
-##  for i=1:nFreq
-##    y = (raw(i,1)+j*raw(i,2))./(raw(i,3)+j*raw(i,4));
-##    s21[i]=y;
-##  endfor
-  % Finally, Close the port
-  fclose(serialPort);
-endfunction
-
 % Load the package
 pkg load instrument-control
 % Check if serial support exists
@@ -58,8 +22,12 @@ endif
 % If you wish to hardcode fMin, fMax, and Nfreq do it here, and you won't
 % be bothered with entering it everytime.
 if(!exist("fMin","var"))
-  sweep = inputdlg({"fMin","fMax","N"},"Frequency Sweep",[1 10; 1 10; 1 3])
-    if (isempty(sweep))
+  sweep = inputdlg({"fMin","fMax","N"},"Frequency Sweep",[1 10; 1 10; 1 3]);
+    if isempty(sweep)
+      fMin = 1.e6;
+      fMax = 100.e6;
+      nFreq = 100;
+    elseif (isempty(sweep{1})||isempty(sweep{2})||isempty(sweep{3}))
       fMin = 1.e6;
       fMax = 100.e6;
       nFreq = 100;
@@ -73,15 +41,20 @@ endif
 df = (fMax-fMin)/nFreq;
 f=fMin:df:fMax-df;
 
-switch (questdlg("Do you wish to load a calibration from disk?"))
+switch (questdlg("Do you wish to load a calibration from disk?"));
   case 'Yes'
-    calFile = uigetfile()
+    calFile = uigetfile();
     load calFile f,H1thru;
   case 'No'  
-    msgbox("Connect the through connection and hit okay.")
+    msgbox("Connect the through connection and hit okay.");
     H1thru = readVNA(fMin, fMax, nFreq);
+    switch (questdlg("Do you wish to save a calibration from disk?"));
+      case 'Yes'
+        calFile = uiputfile();
+        save calFile f,H1thru;
+    end    
 end
-msgbox("Now connect your DUT, and push okay.")
+msgbox("Now connect your DUT, and push okay.");
 H1comb = readVNA(fMin, fMax, nFreq);
 H1dut = H1comb./H1thru;
 % Assume this is for S21.
